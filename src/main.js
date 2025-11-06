2159ff26645aedfe7070e9a3f82b09816f8f8d9d
Garv bansal <garv.bansal2023@vitstudent.ac.in>
2025-11-06 08:39:25 +0530

    Fix: Remove duplicate initCursorGlow function declaration

--- src/main.js
+++ src/main.js
@@ -545,32 +545,3 @@ function initCertificateModal() {
     if (e.key === 'ArrowRight') nextImage()
   })
 }
-
-// Cursor Glow Effect
-function initCursorGlow() {
-  const cursorGlow = document.createElement('div')
-  cursorGlow.className = 'cursor-glow'
-  document.body.appendChild(cursorGlow)
-
-  let mouseX = 0
-  let mouseY = 0
-  let cursorX = 0
-  let cursorY = 0
-
-  document.addEventListener('mousemove', (e) => {
-    mouseX = e.clientX
-    mouseY = e.clientY
-  })
-
-  function animate() {
-    cursorX += (mouseX - cursorX) * 0.1
-    cursorY += (mouseY - cursorY) * 0.1
-
-    cursorGlow.style.left = cursorX + 'px'
-    cursorGlow.style.top = cursorY + 'px'
-
-    requestAnimationFrame(animate)
-  }
-
-  animate()
-}
